e3fc0c70bbcf5e301067fd0d97351c9c678a1c4e
DarkZEKI <81875205+DarkZEKI@users.noreply.github.com>
2021-04-03 13:47:30 -0600

    Update magernulis3.js

--- plugins/magernulis3.js
+++ plugins/magernulis3.js
@@ -8,7 +8,7 @@ let handler  = async (m, { conn, text }) => {
   let tgl = d.toLocaleDateString('id-Id')
   let hari = d.toLocaleDateString('id-Id', { weekday: 'long' })
  text,
-await conn.sendFile(m.chat, global.API('xteam', '/magernulis3', { text, }, 'APIKEY'), 'nulis.jpg', 'Aquí... Tu escritura.\n*Sigueme en Instagram: @mikey69yt*', m)
+await conn.sendFile(m.chat, global.API('xteam', '/magernulis3', { text, }, 'APIKEY'), 'nulis.jpg', 'Aquí... Tu escritura.\n*Sigueme en Instagram: @Proximamente??*', m)
 }
 handler.help = ['magern'].map(v => v + 'ulis3 (texto)')
 handler.tags = ['nulis']
