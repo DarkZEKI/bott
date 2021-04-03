a0850ec1a50f248ba4d4a8f3a87befeaa5071ecf
DarkZEKI <81875205+DarkZEKI@users.noreply.github.com>
2021-04-03 13:44:03 -0600

    Update creador.js

--- plugins/creador.js
+++ plugins/creador.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-  this.sendContact(m.chat, '34698901397', 'Mikey69YT', m)
+  this.sendContact(m.chat, '552197001-9765', 'DarkZE', m)
   //this.sendContact(m.chat, 'TU NUMERO', 'TU NOMBRE', m)
 }
 handler.help = ['owner', 'creator']
