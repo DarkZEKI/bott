fc802df7db08e008ac124dbb9549fa8b9dbf06dd
DarkZEKI <81875205+DarkZEKI@users.noreply.github.com>
2021-04-04 15:48:23 -0500

    Update text3d.js

--- plugins/text3d.js
+++ plugins/text3d.js
@@ -4,7 +4,7 @@ if (!text) return conn.reply(m.chat, 'Introduzca el texto que desee', m)
 if (text > 10) return conn.reply(m.chat, '*Texto demasiado largo!*\n_Maximo 10 letras!_', m)
 let link = 'https://api.zeks.xyz/api/text3d?text=' + text + '&apikey=apivinz'
 conn.sendFile(m.chat, link, 'Mikey Mods.png', '_Al toque mi rey_', m)
-conn.fakeReply(m.chat, '*_Espere mientras se crea..._*','0@s.whatsapp.net','MK-Bot:))')
+conn.fakeReply(m.chat, '*_Espere mientras se crea..._*','0@s.whatsapp.net',':))')
 }
 handler.help = ['3d <texto>']
 handler.tags = ['cfoto']
