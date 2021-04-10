2c246d8778d1b81e2d8f5ff8b1d0674af2d11f0e
DarkZEKI <81875205+DarkZEKI@users.noreply.github.com>
2021-04-10 01:14:53 -0500

    Update unbanchat.js

--- plugins/unbanchat.js
+++ plugins/unbanchat.js
@@ -1,1 +1,21 @@
 
+let handler = async (m, { conn }) => {
+
+  global.DATABASE._data.chats[m.chat].isBanned = false
+
+  m.reply('Hecho!')
+
+}
+
+handler.help = ['unbanchat']
+
+handler.tags = ['owner']
+
+handler.command = /^unbanchat$/i
+
+handler.owner = true
+
+handler.mod = true
+
+module.exports = handler
+
